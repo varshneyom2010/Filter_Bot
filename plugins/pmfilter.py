65f754185595e67924b016c196e9451a4b212e7c
varshneyom2010 <varshneyom2010@gmail.com>
2026-06-12 18:56:16 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1960,7 +1960,7 @@ async def auto_filter(client, msg, spoll=False):
                         sent = await message.reply_photo(photo=photo, caption=cap)
                     except Exception as e:
                         photo = imdb.get('poster')
-                    sent = await rating=imdb['rating'],message.reply_photo(photo=photo, caption=cap)
+                    sent = await message.reply_photo(photo=photo, caption=cap)
                     if m:
                         await m.delete()
                 except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
