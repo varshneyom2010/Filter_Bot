7349f0df807cfa6c0ddb03eac90e8b185d73809d
varshneyom2010 <varshneyom2010@gmail.com>
2026-06-12 19:43:04 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1950,7 +1950,6 @@ async def auto_filter(client, msg, spoll=False):
                     for idx, file in enumerate(files, start=1):
                         cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
                     sent = None
-    sent = None
     try:
         if imdb and imdb.get('poster'):
             try:
