cc5beac8f69a78ea24336cbf5e19f1f3898ac215
varshneyom2010 <varshneyom2010@gmail.com>
2026-06-15 15:00:42 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1940,9 +1940,9 @@ async def auto_filter(client, msg, spoll=False):
                         cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
             else:
                 if settings.get('button'):
-                    cap = f"<b>{file.file_name}</b>\n\n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : <a href='https://t.me'>Filmy Duniya</a>"
+                    cap = f"<b>{file.file_name}</b>\n\n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : <a href='https://t.meFilmy_DuniyaBot'>Filmy Duniya</a>"
                 else:
-                    cap = f"<b>{file.file_name}</b>\n\n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : <a href='https://t.me'>Filmy Duniya</a>"
+                    cap = f"<b>{file.file_name}</b>\n\n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : <a href='https://t.meFilmy_DuniyaBot'>Filmy Duniya</a>"
 
                     for idx, file in enumerate(files, start=1):
                         cap += f"<b>\n{idx}. <a href='https://telegram.me/{temp.U_NAME}?start=file_{message.chat.id}_{file.file_id}'>[{get_size(file.file_size)}] {clean_filename(file.file_name)}\n</a></b>"
