3cb4acab970b9080139824d947284750ca705c98
varshneyom2010 <varshneyom2010@gmail.com>
2026-06-15 15:32:36 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -295,7 +295,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/Filmy_DuniyaBot">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/Filmy_DuniyaBot">[ OWNER ]</a></b>"""
+    CAPTION = """⚜️ Powered By : <a href="https://t.me/Filmy_DuniyaBot">[ OWNER ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
