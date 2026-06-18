7bb3689f8eae4e71190348f8d6f251268d528844
varshneyom2010 <varshneyom2010@gmail.com>
2026-06-18 13:41:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -108,7 +108,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/hdmoviezoneeeee') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Filmy_Cinema_Hub') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Filmy_DuniyaBot') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/hd_movie_zonee') # Update channel link for the bot
 
